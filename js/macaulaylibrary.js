b79f8f6d8df0d63a0ef608db7e50968983ad2a09
Raphaël Nussbaumer <rafnuss@gmail.com>
2020-10-26 13:57:26 +0100

    update api for 2400w macaulaylibrary.js

--- js/macaulaylibrary.js
+++ js/macaulaylibrary.js
@@ -3,7 +3,7 @@
 var url = new URL(window.location.href);
 document.querySelector('.UnorderedList').insertAdjacentHTML('beforeend','\
   <li>\
-    <a class="Button--link" href="https://test.cdn.download.ams.birds.cornell.edu/api/v1'+url.pathname+'/2400" download="">\
+    <a class="Button--link" href="https://cdn.download.ams.birds.cornell.edu/api/v1'+url.pathname+'/2400" download="">\
       <svg class="Icon Icon--jumpDown" role="presentation"><use xlink:href="#Icon--jumpDown"></use></svg>\
 			<span>Download 2400</span>\
 		</a>\
