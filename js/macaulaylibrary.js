944a6e3dc9e046b57c4510d27b6f522339b3a83a
rafnuss <rafnuss@gmail.com>
2023-01-13 13:51:03 -0500

    Update macaulaylibrary.js

--- js/macaulaylibrary.js
+++ js/macaulaylibrary.js
@@ -1,11 +1,6 @@
-'use strict';
-
 var url = new URL(window.location.href);
-document.querySelector('.UnorderedList').insertAdjacentHTML('beforeend', '\
-  <li>\
-    <a class="Button--link" href="https://cdn.download.ams.birds.cornell.edu/api/v1'+ url.pathname + '/2400" download="">\
-      <svg class="Icon Icon--jumpDown" role="presentation"><use xlink:href="#Icon--jumpDown"></use></svg>\
-			<span>Download 2400</span>\
-		</a>\
-	</li>')
-
+document.querySelector('.actions').insertAdjacentHTML('beforeend', `
+    <a class="Button--link" href="https://cdn.download.ams.birds.cornell.edu/api/v1${url.pathname}/2400" target="_blank">
+      <svg class="Icon Icon--jumpDown" role="presentation"><use xlink:href="#Icon--jumpDown"></use></svg>
+			<span>Download 2400</span>
+		</a>`)
