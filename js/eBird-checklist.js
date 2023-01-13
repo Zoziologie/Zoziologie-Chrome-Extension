34c183d5e94bc66e0b0e2d4dc5fe6e1f3c665dc5
rafnuss <rafnuss@gmail.com>
2023-01-13 13:45:31 -0500

    Update eBird-checklist.js

--- js/eBird-checklist.js
+++ js/eBird-checklist.js
@@ -1,27 +1,25 @@
-'use strict';
-var script = document.createElement('script');
-script.type = 'text/javascript';
-script.innerText = '\
-function copyChecklistLinkSpecies(id){\
-  text = window.location.href.split("#")[0] + "#" + id;\
-  window.location.href = text; \
-  dummy = document.createElement("textarea");\
-  document.body.appendChild(dummy);\
-  dummy.value = text;\
-  dummy.select();\
-  document.execCommand("copy");\
-  document.body.removeChild(dummy);\
-  alert("Link copied to clipboad: "+text)\
-}\
-'
-document.getElementsByTagName('head')[0].appendChild(script);
-
 document.querySelectorAll('.Observation').forEach(function (e) {
-  e.getElementsByClassName('Observation-species')[0].getElementsByClassName('Heading')[0].insertAdjacentHTML('beforeend', '\
-  <a class="u-inset-xs" onclick="copyChecklistLinkSpecies(\''+ e.id + '\')">\
-						<svg class="Icon Icon--link Icon--sm" role="presentation"><use xlink:href="#Icon--link"></use></svg>\
-						<span class="is-visuallyHidden">Copy link</span>\
-					</a>')
+  e.
+    insertAdjacentHTML('beforeend', `
+  <div>
+  <a class="zoziology-ebird-link" value="${e.id}">
+						<svg class="Icon Icon--link Icon--sm" role="presentation"><use xlink:href="#Icon--link"></use></svg>
+						<span class="is-visuallyHidden">Copy link</span>
+					</a>
+          </div>`)
 });
 
-
+document.addEventListener("click", function (e) {
+  const target = e.target.closest(".zoziology-ebird-link");
+  console.log(target)
+  if (target) {
+    const text = window.location.href.split("#")[0] + "#" + target.getAttribute("value");
+    if (document.querySelector('#copied')) {
+      document.querySelector('#copied').remove()
+    }
+    navigator.clipboard.writeText(text).then(function () {
+      //alert('URL copied to clipboard: ' + text);
+      e.target.insertAdjacentHTML('afterend', '<span id="copied" style="color: #1c6900;font-size: .88rem;"><svg alt="" aria-hidden="true" class="Icon Icon--check Icon--sm"><use xlink:href="#Icon--check"></use></svg><span style="margin-left:2px;">Copied to clipboard.</span></span>')
+    });
+  }
+});
